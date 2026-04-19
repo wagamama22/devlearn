3055a81e154466714dff6ed9e97ac35ececb9748
wagamama22 <wagajob@gmail.com>
2026-04-19 18:48:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DevLearn',
+  webDir: 'dist'
+};
+
+export default config;
